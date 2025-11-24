8000ec3e7c6f75edac10928433538dce5e5ff691
Naveen <naveenpunchihewa9@gmail.com>
2025-11-24 21:02:24 +0530

    exe-cont

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,8 @@
-import express from "express";
-import notesRoutes from "./routes/notesRoutes.js";
+// import express from "express";
+const express = require("express");
 
-const app = express();
+const app = express()
 
-app.use("/api/notes", notesRoutes);
-   
 app.listen(5001, () => {
-    console.log("Server started on PORT: 5001");
-})
+  console.log("Server is running on port 5001")
+});
