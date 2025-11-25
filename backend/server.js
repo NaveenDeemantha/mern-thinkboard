6bce7385d1f57fac1a0e48d8b21a4fb7e8ec68f1
Naveen <naveenpunchihewa9@gmail.com>
2025-11-25 21:40:15 +0530

    node issue fixed

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
-// import express from "express";
-const express = require("express");
+import express from 'express';
+import notesRoutes from './routes/notesRoutes.js';
 
-const app = express()
+const app = express();
+
+app.use("/api/notes", notesRoutes);
 
 app.listen(5001, () => {
-  console.log("Server is running on port 5001")
+    console.log("Server is running on port: 5001");
 });
